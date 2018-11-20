d7f3013c3074d80f64f9c518457d919ff1e23902
Ashish Gupta <ashish.gupta1@sc.com>
2018-11-20 20:02:45 +0530

    created careers component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, ExtraOptions } from '@angular/router';
 import { HomeComponent } from './pages/home-component/home.component';
+import { CareerComponent } from './pages/career-component/career.component'
 
 const routes: Routes = [
 
-  {
-    path: '', component: HomeComponent,
-  },
+  { path: '', component: HomeComponent },
+  { path: 'careers', component: CareerComponent },
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home-component/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CareerComponent } from './pages/career-component/career.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CareerComponent
   ],
   imports: [
     BrowserModule,

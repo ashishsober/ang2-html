d8050519ea6706b43b4e4f375e0bfdc1922ecb71
Ashish Gupta <ashish.gupta1@sc.com>
2018-11-20 21:27:56 +0530

    contact component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, ExtraOptions } from '@angular/router';
 import { HomeComponent } from './pages/home-component/home.component';
 import { CareerComponent } from './pages/career-component/career.component'
+import { ContactComponent } from './pages/contact-component/contact.component';
 
 const routes: Routes = [
 
   { path: '', component: HomeComponent },
   { path: 'careers', component: CareerComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: '**', redirectTo: '', pathMatch: 'full' }
-
-
 ];
 // const config: ExtraOptions = {
 //   useHash: true,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home-component/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CareerComponent } from './pages/career-component/career.component'
+import { ContactComponent } from './pages/contact-component/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CareerComponent
+    CareerComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,

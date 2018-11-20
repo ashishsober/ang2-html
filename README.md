1b673f65763fa6e2c66d4d94316db305ba00a49b
Ashish Gupta <ashish.gupta1@sc.com>
2018-11-20 16:16:14 +0530

    first commit

--- README.md
+++ README.md
@@ -25,3 +25,18 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+
+## Important points 
+
+* Bootstrap-----4.1.3
+* Created EC2 instance of Ubuntu 18.0 image.
+* Only using SVG icons
+* using node version 9.5.0
+
+* To login from the terminal 
+      ssh -i /Users/ashishgupta/Desktop/Vivek\ Sir/vrdNetworkKeyPair.pem ubuntu@ec2-3-16-206-69.us-east-2.compute.amazonaws.com
+
+* To run the app from AWS instance,which is alredy running no need of running again
+
+            sudo nohup http-server -p 80 &
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { HostListener, Inject } from "@angular/core";
+import { DOCUMENT } from '@angular/platform-browser';
 
+declare const window: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  bannerColorToBlack:boolean;
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (number > 100) {
+      this.bannerColorToBlack = true;
+    } else {
+      this.bannerColorToBlack = false;
+    }
+  }
+
 }

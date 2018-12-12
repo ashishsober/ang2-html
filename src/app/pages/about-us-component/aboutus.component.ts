20c7c84d8d0654823676a0293e0bfef8bcd1687b
Ashish Gupta <ashish.gupta1@sc.com>
2018-12-12 22:15:24 +0530

    added images

--- src/app/pages/about-us-component/aboutus.component.ts
+++ src/app/pages/about-us-component/aboutus.component.ts
@@ -8,10 +8,11 @@ import { Component } from '@angular/core';
 export class AboutUsComponent {
 
   slides = [
-    {img: "http://placehold.it/350x150/000000"},
     {img: "assets/vrd-group1.jpeg"},
-    {img: "https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"},
-    {img: "https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg"}
+    {img: "assets/vrd-group2.jpeg"},
+    {img: "assets/vrd-group3.jpeg"},
+    {img: "assets/vrd-group4.jpg"}
+    // {img: "assets/vrd-group5.jpg"}
   ];
 
 }
--- src/app/pages/career-component/career.component.ts
+++ src/app/pages/career-component/career.component.ts
@@ -8,7 +8,16 @@ import { NgForm } from '@angular/forms';
 })
 export class CareerComponent {
   contactModal = new contactModal('','',null,'','','');
-
+  genderboxData: Array<any> = [
+    {
+      "CODE_DESC": "Male",
+      "CODE_VALUE": "male"
+    },
+    {
+      "CODE_DESC": "Female",
+      "CODE_VALUE": "female"
+    }
+    ];
   onSubmit({ form, valid }: { form:NgForm , valid: boolean }){
     if(valid){
       console.log("submit career"+form.value);

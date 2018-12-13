6913b9a6208a9ce68028015c083e9fdf7b8adc04
Ashish Gupta <ashish.gupta1@sc.com>
2018-12-13 19:33:14 +0530

    updated management

--- src/app/pages/management-component/management.component.ts
+++ src/app/pages/management-component/management.component.ts
@@ -10,7 +10,7 @@ export class ManagementComponent {
     "name":"Vivek Kumar",
     "position":"Chairman & CEO",
     "profileImage":"assets/vivek_sir.jpeg",
-    "discription":"As Chairman of VRD NETWORK , as well as leading the company's product development and technology strategy. Under his leadership, VRD NETWORK has broadened its offerings while maintaining a culture of strong innovation. His background uniquely prepares him to lead VRD NETWORK toward technological solutions that focus on users.",
+    "discription":"As Chief Executive Officer of VRD NETWORK, Under his leadership VRD NETWORK has broadened its offerings while maintaining a culture of strong innovation. His background uniquely prepares him to lead VRD NETWORK towards technological solutions that focus on users.",
     "emailid":"director@vrdnetwork.com"
   },
   {
@@ -21,10 +21,10 @@ export class ManagementComponent {
     "emailid":"hr@vrdnetwork.com"
   },
   {
-    "name":"K MOHIT",
-    "position":"Sales Operations and Business Development Officer",
+    "name":"PRANJUL",
+    "position":"HR Executive",
     "profileImage":"assets/user-tie-solid.svg",
-    "discription":"He is responsible for the company's marketing efforts globally. His marketing programs focus on the go-to-market strategy and adoption of all of our products, from consumer offerings to business services. He is responsible for the company's Business Development activities specially in United States of America. ",
-    "emailid":""
+    "discription":"Handles payroll, benefits, employee relations and training. Responsible for planning , directing & coordinating administrative functions.",
+    "emailid":"pranjul@vrdnetwork.com "
   }]
 }

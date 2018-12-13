1736362eb10a28c23f4fab9aca7143e9d4bc4eb1
Ashish Gupta <ashish.gupta1@sc.com>
2018-12-13 11:12:17 +0530

    change the banner for all the services

--- src/app/pages/workflow-component/workflow.component.ts
+++ src/app/pages/workflow-component/workflow.component.ts
@@ -8,7 +8,9 @@ import { Component } from '@angular/core';
 export class WorkflowComponent {
   slides = [
     {img: "assets/banner1.jpg"},
-    {img: "assets/banner2.jpg"}
+    {img: "assets/banner2.jpg"},
+    {img: "assets/banner3.jpg"},
+    {img: "assets/banner4.jpg"}
   ];
 }
 

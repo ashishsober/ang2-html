8021485c33f98699f5a9ecf3c95e5cef60436fe2
Ashish Gupta <ashish.gupta1@sc.com>
2018-12-22 01:44:53 +0530

    code reactoring

--- src/app/pages/job-board/job-board.component.ts
+++ src/app/pages/job-board/job-board.component.ts
@@ -6,27 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./job-board.component.scss']
 })
 export class JobBoardComponent {
-  jobData=[
+  jobData = [
     {
-      title:'SAP PP-PI Consultant',location:'Bengaluru',jobType:'Permanent Position',experience:'Requires 7+ years’ experience in delivering solutions within the SAP PP-PI module.Able to setup all PP-PI master data',
-    requirement:['7+ years of SAP configuration for PP-PI','Experience in setting up Master data for: PPDS, Planning, S&OP, LTP',
-    'Chemical Industry experience preferred and a plus','Experience in MRP processes and planning, make to order and make to stock scenarios',
-  'Knowledge of APO planning (PPDS), Plant maintenance, QM and MM a plus','Batch management knowledge required ',
-'Material Forecasting and Demand Management a plus','LTP, MPS and SOP a plus','Excellent verbal and written communication skills']
-},
-{
-  title:'Syteline Functional Consultant',location:'Bengaluru',jobType:'Permanent Position',experience:'We are looking for Syteline functional consultant to join our Syteline team and support us in our existing requirement.4+ years of Syteline Experience is required.',
-    requirement:[]
-},
-{
-  title:'Power BI Consultant',location:'Bengaluru',jobType:'Contract',experience:'We are looking for a Power BI Consultant who has good experience to build complex data model in power BI and develop advance report/Cubes in Power BI.',
-    requirement:[]
-},
-{
-  title:'.Net Consultant',location:'Bengaluru',jobType:'Permanent Position',experience:'4+ Years',
-    requirement:['Web applications configurations in cloud and user support for the application','Develop reports using SQL Server Reporting Services, Power BI and other reporting tools',
-  'Configure and maintain Microsoft Azure PaaS environments','Microsoft Azure cloud platform technologies','Visual Studio 2013 or later',
-'ASP.NET, C#  and T-SQL.','Solid understanding of software testing, continuous integration & continuous delivery','Web technologies – HTML, CSS, JS']
-}
+      title: 'SAP PP-PI Consultant',
+      location: 'Bengaluru',
+      jobType: 'Permanent Position',
+      experience: 'Requires 7+ years’ experience in delivering solutions within the SAP PP-PI module.Able to setup all PP-PI master data',
+      requirement: ['7+ years of SAP configuration for PP-PI', 'Experience in setting up Master data for: PPDS, Planning, S&OP, LTP',
+        'Chemical Industry experience preferred and a plus', 'Experience in MRP processes and planning, make to order and make to stock scenarios',
+        'Knowledge of APO planning (PPDS), Plant maintenance, QM and MM a plus', 'Batch management knowledge required ',
+        'Material Forecasting and Demand Management a plus', 'LTP, MPS and SOP a plus', 'Excellent verbal and written communication skills'
+      ]
+    },
+    {
+      title: 'Syteline Functional Consultant',
+      location: 'Bengaluru',
+      jobType: 'Permanent Position',
+      experience: 'We are looking for Syteline functional consultant to join our Syteline team and support us in our existing requirement.4+ years of Syteline Experience is required.',
+      requirement: []
+    },
+    {
+      title: 'Power BI Consultant',
+      location: 'Bengaluru',
+      jobType: 'Contract',
+      experience: 'We are looking for a Power BI Consultant who has good experience to build complex data model in power BI and develop advance report/Cubes in Power BI.',
+      requirement: []
+    },
+    {
+      title: '.Net Consultant',
+      location: 'Bengaluru',
+      jobType: 'Permanent Position',
+      experience: '4+ Years',
+      requirement: ['Web applications configurations in cloud and user support for the application', 'Develop reports using SQL Server Reporting Services, Power BI and other reporting tools',
+        'Configure and maintain Microsoft Azure PaaS environments', 'Microsoft Azure cloud platform technologies', 'Visual Studio 2013 or later',
+        'ASP.NET, C#  and T-SQL.', 'Solid understanding of software testing, continuous integration & continuous delivery', 'Web technologies – HTML, CSS, JS'
+      ]
+    }
   ]
 }
--- src/app/pages/management-component/management.component.ts
+++ src/app/pages/management-component/management.component.ts
@@ -6,25 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./management.component.scss']
 })
 export class ManagementComponent {
-  managementList=[{
-    "name":"Vivek Kumar",
-    "position":"Chairman & CEO",
-    "profileImage":"assets/vivek_sir.jpeg",
-    "discription":"As Chief Executive Officer of VRD NETWORK, Under his leadership VRD NETWORK has broadened its offerings while maintaining a culture of strong innovation. His background uniquely prepares him to lead VRD NETWORK towards technological solutions that focus on users.",
-    "emailid":"director@vrdnetwork.com"
+  managementList = [{
+    name: "Vivek Kumar",
+    position: "Chairman & CEO",
+    profileImage: "assets/vivek_sir.jpeg",
+    discription: "As Chief Executive Officer of VRD NETWORK, Under his leadership VRD NETWORK has broadened its offerings while maintaining a culture of strong innovation. His background uniquely prepares him to lead VRD NETWORK towards technological solutions that focus on users.",
+    emailid: "director@vrdnetwork.com"
   },
   {
-    "name":"P. VENKAT",
-    "position":"Associate Director -  Delivery, Resource Management",
-    "profileImage":"assets/user-tie-solid.svg",
-    "discription":"Oversee , implement and manage the Recruitment process of compamy. Responsible for the planning, design and implementation of cost effective employee benefit programs consistent with the Company’s objectives for employees.",
-    "emailid":"hr@vrdnetwork.com"
+    name: "P. VENKAT",
+    position: "Associate Director -  Delivery, Resource Management",
+    profileImage: "assets/user-tie-solid.svg",
+    discription: "Oversee , implement and manage the Recruitment process of compamy. Responsible for the planning, design and implementation of cost effective employee benefit programs consistent with the Company’s objectives for employees.",
+    emailid: "hr@vrdnetwork.com"
   },
   {
-    "name":"PRANJUL",
-    "position":"HR Executive",
-    "profileImage":"assets/user-tie-solid.svg",
-    "discription":"Handles payroll, benefits, employee relations and training. Responsible for planning , directing & coordinating administrative functions.",
-    "emailid":"pranjul@vrdnetwork.com "
+    name: "PRANJUL",
+    position: "HR Executive",
+    profileImage: "assets/user-tie-solid.svg",
+    discription: "Handles payroll, benefits, employee relations and training. Responsible for planning , directing & coordinating administrative functions.",
+    emailid: "pranjul@vrdnetwork.com "
   }]
 }

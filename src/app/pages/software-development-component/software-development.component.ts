53100235e29cfa89a054c6fe22d9b33bac25ba15
Ashish Gupta <ashish.gupta1@sc.com>
2018-12-27 12:45:41 +0530

    modularize

--- src/app/pages/software-development-component/software-development.component.ts
+++ src/app/pages/software-development-component/software-development.component.ts
@@ -12,5 +12,21 @@ export class SoftwareDevelopmentComponent {
     {img: "assets/banner3.jpg",text:'Responsive websites'},
     {img: "assets/banner12.jpg",text:''}
   ];
+
+  techno_learn =[
+    {img:"assets/tech-icons/angular6.png",title:"Angular 6"},
+    {img:"assets/tech-icons/react.png",title:"React"},
+    {img:"assets/tech-icons/typescript.png",title:"Typescript"},
+    {img:"assets/tech-icons/sapui5.png",title:"SAPUI5"},
+    {img:"assets/tech-icons/firebase.png",title:"Firebase"},
+    {img:"assets/tech-icons/bootstrap.png",title:"Bootstrap"},
+    {img:"assets/tech-icons/html5.png",title:"HTML5"},
+    {img:"assets/tech-icons/css.png",title:"CSS 3"},
+    {img:"assets/tech-icons/sass.png",title:"SASS"},
+    {img:"assets/tech-icons/nodejs.png",title:"NodeJs"},
+    {img:"assets/tech-icons/mongodb.png",title:"Mongo DB"},
+    {img:"assets/tech-icons/java.png",title:"Java"},
+    {img:"assets/tech-icons/aws.png",title:"AWS"}
+  ]
 }
 

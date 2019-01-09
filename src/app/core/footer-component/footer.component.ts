e7f9ac8ada87db010bb6e621c39591a396c0c9a1
Ashish Gupta <ashish.gupta1@sc.com>
2019-01-09 19:26:38 +0530

    ddfdf

--- src/app/core/footer-component/footer.component.ts
+++ src/app/core/footer-component/footer.component.ts
@@ -1,48 +1,9 @@
 import { Component } from '@angular/core';
-import { DataService } from '../data.service';
-import { AlertDialogComponent } from '../../core/dialog/alert-dialog.component';
-import { MatDialog, MatDialogRef } from '@angular/material';
-
 @Component({
   selector: 'ngx-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-  alertDialogRef: MatDialogRef<AlertDialogComponent>;
-  constructor(private dataService:DataService,
-              private dialog: MatDialog){}
-
-  gsubmit() {
-    this.dataService.getGoogleAuth()
-      .then((result) => {
-          this.saveUserCall(result.user);        
-      },(err) => {
-        console.log("google auth error--"+err);
-        this.errorModal(err);
-      });
-  }
-  
-  saveUserCall(userData:any) {
-    this.dataService.postEmployee(userData).subscribe((result) => {
-      console.log("my result===="+result);
-          // sessionStorage.setItem('user_uid', result.user.uid);
-          // sessionStorage.setItem('user_photoUrl', result.user.photoURL);
-          // sessionStorage.setItem('user_emalid', result.user.email);  
-    },(err) => {
-      console.log(err);
-      this.errorModal(err);
-    });
-  }
-
-  errorModal(err: any) {
-    this.alertDialogRef = this.dialog.open(AlertDialogComponent, {
-      hasBackdrop: true,
-      height: '316px',
-      width: '874px',
-      disableClose: true,
-      data: err
-    });
-  }
-  //uid---Zg22X8LOQihr6gyBDLy7Lhy9HG83
+  constructor(){}
 }
--- src/app/core/header-component/header.component.ts
+++ src/app/core/header-component/header.component.ts
@@ -1,8 +1,10 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter ,NgZone} from '@angular/core';
 import { HostListener, Inject } from "@angular/core";
 import { DOCUMENT } from '@angular/platform-browser';
 import { Router } from '@angular/router'
-
+import { DataService } from '../data.service';
+import { AlertDialogComponent } from '../../core/dialog/alert-dialog.component';
+import { MatDialog, MatDialogRef } from '@angular/material';
 
 
 declare const window: any;
@@ -19,10 +21,12 @@ export class HeaderComponent {
   displayCompanyMenu:boolean=false;
   displayServiceMenu:boolean=false;
   displayRecruitMenu:boolean=false;
-  
+  alertDialogRef: MatDialogRef<AlertDialogComponent>;
+  loginInBtn:string = "Login"
   @Output() right50Event = new EventEmitter<boolean>();  
 
-  constructor(private router:Router){}
+  constructor(private router:Router,private dataService:DataService,
+    private dialog: MatDialog,private zone:NgZone,){}
 
   @HostListener("window:scroll", [])
   onWindowScroll() {
@@ -59,5 +63,47 @@ export class HeaderComponent {
     this.right50 = false;   //emit from here
     this.right50Event.emit(this.right50);
     this.router.navigate([state]);
+  }
+
+  //google auth call
+  gsubmit(value:string) {
+    if(value === 'Login') {
+      this.dataService.getGoogleAuth()
+      .then((result) => {
+          sessionStorage.setItem('photoUrl', result.user.photoURL);
+          sessionStorage.setItem('emalid', result.user.email);  
+          this.saveUserCall(result.user);        
+      },(err) => {
+        this.errorModal(err);
+      });
+    } else {
+      alert(value);
+    }
+   
+  }
+  
+
+  //save user and accestoken call for the database
+  saveUserCall(userData:any) {
+    this.dataService.postEmployee(userData).subscribe((result) => {
+          sessionStorage.setItem('user_uid', result.uid);
+          sessionStorage.setItem('accessToken', result.accessToken);
+          this.zone.run(() =>{
+            this.loginInBtn = "Logout";
+         });  
+    },(err) => {
+      console.log(err);
+      this.errorModal(err);
+    });
+  }
+
+  errorModal(err: any) {
+    this.alertDialogRef = this.dialog.open(AlertDialogComponent, {
+      hasBackdrop: true,
+      height: '316px',
+      width: '874px',
+      disableClose: true,
+      data: err
+    });
   }
 }

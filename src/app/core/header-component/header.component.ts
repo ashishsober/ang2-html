764350db1b3cb3b19228d3534f3b3c966fd41ddd
Ashish Gupta <ashish.gupta1@sc.com>
2019-01-10 13:04:11 +0530

    code cleaning

--- src/app/core/header-component/header.component.ts
+++ src/app/core/header-component/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter} from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { HostListener, Inject } from "@angular/core";
 import { DOCUMENT } from '@angular/platform-browser';
 import { Router } from '@angular/router'
@@ -11,19 +11,16 @@ declare const window: any;
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  bannerColorToBlack:boolean;
-  displayBlock:boolean=false;
-  right50:boolean=false;
-  positionFixed:boolean=false;
-  displayCompanyMenu:boolean=false;
-  displayServiceMenu:boolean=false;
-  displayRecruitMenu:boolean=false;
- 
-  @Output() right50Event = new EventEmitter<boolean>();  
+  bannerColorToBlack: boolean;
+  displayBlock: boolean = false;
+  right50: boolean = false;
+  positionFixed: boolean = false;
+  displayCompanyMenu: boolean = false;
+  displayServiceMenu: boolean = false;
+  displayRecruitMenu: boolean = false;
+  @Output() right50Event = new EventEmitter<boolean>();
 
-  constructor(private router:Router){}
-
-   
+  constructor(private router: Router) { }
 
   @HostListener("window:scroll", [])
   onWindowScroll() {
@@ -31,7 +28,7 @@ export class HeaderComponent {
     if (number > 20 && window.outerHeight > 375) {
       this.bannerColorToBlack = true;
       this.positionFixed = true;
-    } else if (window.outerHeight <= 375){
+    } else if (window.outerHeight <= 375) {
       this.bannerColorToBlack = false;
       this.positionFixed = false;
     } else {
@@ -40,23 +37,23 @@ export class HeaderComponent {
     }
   }
 
-  sidebar(state:any){
-    if(state ==='close'){
-      this.displayBlock =false;
+  sidebar(state: any) {
+    if (state === 'close') {
+      this.displayBlock = false;
       this.right50 = false;  //emit from here
       this.right50Event.emit(this.right50);
       this.positionFixed = false;
     } else {
-      this.displayBlock =true;
+      this.displayBlock = true;
       this.right50 = true;  //emit from here
       this.right50Event.emit(this.right50);
       this.positionFixed = true;
     }
-   
+
   }
 
-  navigateTo(state:any){
-    this.displayBlock =false;
+  navigateTo(state: any) {
+    this.displayBlock = false;
     this.right50 = false;   //emit from here
     this.right50Event.emit(this.right50);
     this.router.navigate([state]);

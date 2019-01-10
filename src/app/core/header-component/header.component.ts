85492ef56890122c2a356694629bbba535d6323c
Ashish Gupta <ashish.gupta1@sc.com>
2019-01-10 13:02:31 +0530

    created a separate component for the login button

--- src/app/core/header-component/header.component.ts
+++ src/app/core/header-component/header.component.ts
@@ -1,10 +1,7 @@
-import { Component, Output, EventEmitter ,NgZone, OnInit} from '@angular/core';
+import { Component, Output, EventEmitter} from '@angular/core';
 import { HostListener, Inject } from "@angular/core";
 import { DOCUMENT } from '@angular/platform-browser';
 import { Router } from '@angular/router'
-import { DataService } from '../data.service';
-import { AlertDialogComponent } from '../../core/dialog/alert-dialog.component';
-import { MatDialog, MatDialogRef } from '@angular/material';
 
 
 declare const window: any;
@@ -13,7 +10,7 @@ declare const window: any;
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent {
   bannerColorToBlack:boolean;
   displayBlock:boolean=false;
   right50:boolean=false;
@@ -21,33 +18,12 @@ export class HeaderComponent implements OnInit{
   displayCompanyMenu:boolean=false;
   displayServiceMenu:boolean=false;
   displayRecruitMenu:boolean=false;
-  alertDialogRef: MatDialogRef<AlertDialogComponent>;
-  loginInBtn:string;
-  userData = {
-    accessToken:sessionStorage.getItem("accessToken"),
-    uid:sessionStorage.getItem("user_uid")
-  }
+ 
   @Output() right50Event = new EventEmitter<boolean>();  
 
-  constructor(private router:Router,private dataService:DataService,
-    private dialog: MatDialog,private zone:NgZone,){}
+  constructor(private router:Router){}
 
-  ngOnInit(){
-    if(this.userData.accessToken != null || this.userData.uid != null){
-      this.dataService.authenticateEmp(this.userData).subscribe((result) => {
-        if(result.length === 1 ){
-          this.loginInBtn = "Logout";
-        } else {
-           this.loginInBtn = "Login";
-        }
-     },(err) => {
-        console.log(err);
-        this.errorModal(err);
-     });
-    } else {
-      this.loginInBtn = "Login";
-    }
-  } 
+   
 
   @HostListener("window:scroll", [])
   onWindowScroll() {
@@ -84,58 +60,5 @@ export class HeaderComponent implements OnInit{
     this.right50 = false;   //emit from here
     this.right50Event.emit(this.right50);
     this.router.navigate([state]);
-  }
-
-  //google auth call
-  gsubmit(value:string) {
-    if(value === 'Login') {
-      this.dataService.getGoogleAuth()
-      .then((result) => {
-          sessionStorage.setItem('photoUrl', result.user.photoURL);
-          sessionStorage.setItem('emalid', result.user.email);  
-          this.saveUserCall(result.user);        
-      },(err) => {
-        console.log(err);
-        this.errorModal(err);
-      });
-    } else {
-      this.logout();
-    }
-   
-  }
-  
-
-  //save user and accestoken call for the database
-  saveUserCall(userData:any) {
-    this.dataService.postEmployee(userData).subscribe((result) => {
-          sessionStorage.setItem('user_uid', result.uid);
-          sessionStorage.setItem('accessToken', result.accessToken);
-          this.zone.run(() =>{
-            this.loginInBtn = "Logout";
-         });  
-    },(err) => {
-      console.log(err);
-      this.errorModal(err);
-    });
-  }
-
-  logout () {
-    this.dataService.logout(this.userData).subscribe((result) =>{
-      this.loginInBtn = "Login";
-      sessionStorage.clear();
-    },(err)=>{
-      console.log(err);
-      this.errorModal(err);
-    });
-  }
-
-  errorModal(err: any) {
-    this.alertDialogRef = this.dialog.open(AlertDialogComponent, {
-      hasBackdrop: true,
-      height: '316px',
-      width: '874px',
-      disableClose: true,
-      data: err
-    });
   }
 }
---
+++ src/app/core/loginbtn-component/loginbtn.component.ts
@@ -0,0 +1,94 @@
+import { Component, Output, EventEmitter ,NgZone, OnInit} from '@angular/core';
+import { HostListener, Inject } from "@angular/core";
+import { Router } from '@angular/router'
+import { DataService } from '../data.service';
+import { AlertDialogComponent } from '../../core/dialog/alert-dialog.component';
+import { MatDialog, MatDialogRef } from '@angular/material';
+
+@Component({
+  selector: 'ngx-login-btn',
+  templateUrl: './loginbtn.component.html',
+  styleUrls: ['./loginbtn.component.scss']
+})
+export class LoginbtnComponent implements OnInit {
+  alertDialogRef: MatDialogRef<AlertDialogComponent>;
+  loginInBtn:string;
+  userData = {
+    accessToken:sessionStorage.getItem("accessToken"),
+    uid:sessionStorage.getItem("user_uid")
+  }
+  @Output() right50Event = new EventEmitter<boolean>();  
+
+  constructor(private router:Router,private dataService:DataService,
+    private dialog: MatDialog,private zone:NgZone,){}
+
+  ngOnInit(){
+    if(this.userData.accessToken != null || this.userData.uid != null){
+      this.dataService.authenticateEmp(this.userData).subscribe((result) => {
+        if(result.length === 1 ){
+          this.loginInBtn = "Logout";
+        } else {
+           this.loginInBtn = "Login";
+        }
+     },(err) => {
+        console.log(err);
+        this.errorModal(err);
+     });
+    } else {
+      this.loginInBtn = "Login";
+    }
+  } 
+
+
+  //google auth call
+  gsubmit(value:string) {
+    if(value === 'Login') {
+      this.dataService.getGoogleAuth()
+      .then((result) => {
+          sessionStorage.setItem('photoUrl', result.user.photoURL);
+          sessionStorage.setItem('emalid', result.user.email);  
+          this.saveUserCall(result.user);        
+      },(err) => {
+        console.log(err);
+        this.errorModal(err);
+      });
+    } else {
+      this.logout();
+    }
+  }
+  
+
+  //save user and accestoken call for the database
+  saveUserCall(userData:any) {
+    this.dataService.postEmployee(userData).subscribe((result) => {
+          sessionStorage.setItem('user_uid', result.uid);
+          sessionStorage.setItem('accessToken', result.accessToken);
+          this.zone.run(() =>{
+            this.loginInBtn = "Logout";
+         });  
+    },(err) => {
+      console.log(err);
+      this.errorModal(err);
+    });
+  }
+
+  logout () {
+    this.dataService.logout(this.userData).subscribe((result) =>{
+      this.loginInBtn = "Login";
+      sessionStorage.clear();
+    },(err)=>{
+      console.log(err);
+      this.errorModal(err);
+    });
+  }
+
+  errorModal(err: any) {
+    this.alertDialogRef = this.dialog.open(AlertDialogComponent, {
+      hasBackdrop: true,
+      height: '316px',
+      width: '874px',
+      disableClose: true,
+      data: err
+    });
+  }
+}

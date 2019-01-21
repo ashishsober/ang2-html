93bcaf757eaf409fdf9ecddf37165016821439a8
Ashish Gupta <ashish.gupta1@sc.com>
2019-01-21 16:51:56 +0530

    contact component unwanted code removed

--- src/app/pages/contact-component-module/contact.component.ts
+++ src/app/pages/contact-component-module/contact.component.ts
@@ -18,81 +18,11 @@ export class ContactComponent {
   hide: boolean = false;
   showForm = true;
   alertDialogRef: MatDialogRef<AlertDialogComponent>;
-  selectboxData: Array<any> = this.dataService.technologiesSelectboxData;
-  contact_address: Array<any> = this.dataService.contact_address;
+  //selectboxData: Array<any> = this.dataService.technologiesSelectboxData;
+  //contact_address: Array<any> = this.dataService.contact_address;
   showSpinner=false;
 
-  constructor(private dataService: DataService,
-    private dialog: MatDialog) { }
+  constructor() { }
 
-  onselect(event: MatOptionSelectionChange) {
-    if (event.source.selected) {
-      if (event.source.value === 'other') {
-        this.hide = true; //making visible,field should be blank
-        this.contactModal.other = null;
-      } else {
-        this.hide = false;
-        this.contactModal.other = null;
-      }
-    }
-  }
-
-  onSubmit({ form, valid }: { form: NgForm, valid: boolean }) {
-    if (valid) {
-      this.showSpinner=true;
-      this.dataService.postContact(form.value).subscribe((result) => {
-        this.showSpinner=false;
-        this.showForm = false;
-        form.reset();
-      }, err => {
-        this.showSpinner=false;
-        console.log(err);
-        this.errorModal(err);
-      });
-    }
-  }
-
-  log(x: any) {
-    console.log(x);
-  }
-
-  numberval = true;
-
-  onKey(evt: any) {
-    if (this.contactModal.phone_no.toString().length >= 10) {
-      this.numberval = true;
-    }
-  }
-
-  checkServerResponse(appData: any, form: NgForm) {
-    let applicationStatus = appData.application.response_type.toUpperCase(); //info
-    let responseAction = appData.application.response_action.toUpperCase();
-    switch (responseAction) {
-      case 'STOP':
-        this.errorModal(appData);
-        break;
-      case 'CONTINUE':
-      case 'SUCCESS':
-        this.errorModal(appData);
-        form.reset();
-        break;
-      default:
-        //error modal to show  
-        this.errorModal(appData);
-    }
-
-  }
-
-
-  errorModal(err: any) {
-    this.alertDialogRef = this.dialog.open(AlertDialogComponent, {
-      hasBackdrop: true,
-      height: '316px',
-      width: '874px',
-      disableClose: true,
-      data: err
-    });
-  }
-
-
+ 
 }

85ccb8e5941ec24212e6ba04178d3e33ede8cb65
Ashish Gupta <ashish.gupta1@sc.com>
2019-01-28 22:33:06 +0530

    added pic

--- src/app/pages/about-us-component/aboutus.component.ts
+++ src/app/pages/about-us-component/aboutus.component.ts
@@ -8,10 +8,10 @@ import { preserveWhitespacesDefault } from '@angular/compiler';
 })
 export class AboutUsComponent implements OnInit, OnDestroy {
   slides2 = [
-    {img: "assets/banner1.jpg",text:'Software-Development'},
-    {img: "assets/banner2.jpg",text:'Search engine optimization'},
-    {img: "assets/banner3.jpg",text:'Responsive websites'},
-    {img: "assets/banner12.jpg",text:''}
+    {img: "assets/front-images4.jpg",text:''},
+    {img: "assets/front-images.png",text:'Software-Development'},
+    {img: "assets/front-images2.jpg",text:'Search engine optimization'},
+    {img: "assets/front-images3.jpg",text:'Responsive websites'}
   ];
 
   slides = [

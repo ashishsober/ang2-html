ecfd9b21e8fcb9cac645a465c580ce79c970a93b
Ashish Gupta <ashish.gupta1@sc.com>
2019-03-11 00:29:24 +0530

    Update loginbtn.component.ts

--- src/app/core/loginbtn-component/loginbtn.component.ts
+++ src/app/core/loginbtn-component/loginbtn.component.ts
@@ -18,7 +18,7 @@ export class LoginbtnComponent implements OnInit {
     user_data: user_Data;
     @Output() right50Event = new EventEmitter<boolean>();
     userInfoModalComponent: MatDialogRef<UserInfoModalComponent>;
-
+    
     constructor(private router: Router, private dataService: DataService,
         private dialog: MatDialog, private zone: NgZone) {
         var userModal = new user_Data();
@@ -32,10 +32,23 @@ export class LoginbtnComponent implements OnInit {
             }
         });
 
-
+        let obj = {
+            applicants: {},
+            application: {
+              message: "",
+              response_action: ""
+            },
+            client: {
+              uid: this.user_data.uid,
+              accessToken: this.user_data.accessToken,
+              emailId:"",
+              photoUrl:"",
+              displayName:""
+            }
+        };
         if (this.user_data.accessToken != null || this.user_data.uid != null) {
-            this.dataService.authenticateEmp(this.user_data).subscribe((result) => {
-                if (result.responseAction === "info") {
+            this.dataService.authenticateEmp(obj).subscribe((result) => {
+                if (result.application.response_action === "continue") {
                     this.showLogoutButton(result);
                 } else {
                     this.showLoginButton();
@@ -57,8 +70,8 @@ export class LoginbtnComponent implements OnInit {
 
     showLogoutButton(result: any) {
         if (result != 'logout') {
-            let photoUrl = result.photoUrl === undefined ? result.photos[0].value : result.photoUrl;
-            let email = result.emailId === undefined ? result.emails[0].value : result.emailId;
+            let photoUrl = result.client === undefined ? result.photos[0].value  :result.client.photoUrl;
+            let email = result.client === undefined ? result.emails[0].value : result.client.emailId ;
             this.zone.run(() => {
                 this.displayNone = true; //show the user_img
                 this.user_data.photoURL = photoUrl;
